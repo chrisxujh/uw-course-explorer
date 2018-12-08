e8ec3094002a5d6bf36fb9358350f4f77b91d04f
Chris Xu <j352xu@edu.uwaterloo.ca>
2018-12-08 18:05:30 -0500

    Improve footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from '../../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
+  birthday: string;
+  age$: Observable<any>;
+  interval: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.birthday = new Date(1544158800000).toString();
+    this.age$ = Observable.create(observer => {
+      this.interval = setInterval(() => {
+        const timeDiff =
+          Date.parse(new Date().toString()) - Date.parse(this.birthday);
+        const diff = new Date(timeDiff);
+        observer.next({
+          secondsDiff: diff.getUTCSeconds(),
+          minutesDiff: diff.getUTCMinutes(),
+          hoursDiff: diff.getUTCHours(),
+          daysDiff: Math.ceil(timeDiff / (1000 * 3600 * 24))
+        });
+      });
+    });
   }
 
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
 }
