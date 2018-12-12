05cfd323d8d3d78f254b81faf4d89551a7313cd3
Chris Xu <chris.xu03@sap.com>
2018-12-12 15:45:44 -0500

    CSS for paginator; allow paginator to auto resize

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -8,28 +8,30 @@ import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 export class PaginatorComponent implements OnInit {
   @Output() emitter: EventEmitter<any> = new EventEmitter();
   maxIndex: number;
-  indexes: number[];
+  indexes: number[] = null;
+  indexTolerance = 2;
   currentIndex = 1;
 
   constructor() {}
 
   ngOnInit() {
-    this.indexes = Array.from(Array(this.maxIndex).keys()).map(key => key + 1);
+    this.resetIndexes();
   }
 
   @Input()
   set size(s: number) {
     this.maxIndex = s;
   }
+
   next() {
     if (this.currentIndex < this.maxIndex) {
-      this.emitIndex(++this.currentIndex);
+      this.switchIndex(++this.currentIndex);
     }
   }
 
   previous() {
     if (this.currentIndex > 1) {
-      this.emitIndex(--this.currentIndex);
+      this.switchIndex(--this.currentIndex);
     }
   }
 
@@ -37,6 +39,25 @@ export class PaginatorComponent implements OnInit {
     if (index > 0 && index <= this.maxIndex) {
       this.currentIndex = index;
       this.emitIndex(this.currentIndex);
+    }
+    if (this.currentIndex !== 1) {
+      this.resetIndexes();
+    }
+  }
+
+  private resetIndexes() {
+    if (
+      this.indexes === null ||
+      this.currentIndex <= this.indexes[0] ||
+      this.currentIndex >= this.indexes[this.indexes.length - 1]
+    ) {
+      this.indexes = Array.from(Array(this.maxIndex).keys())
+        .map(key => key + 1)
+        .filter(key =>
+          this.currentIndex >= this.indexTolerance
+            ? Math.abs(this.currentIndex - key) <= this.indexTolerance
+            : key <= 2 * this.indexTolerance + 1
+        );
     }
   }
 
