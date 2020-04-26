03a1a52957e3079aa1fceaa873048aa0329540ce
Chris Xu <gzchrisxjh@gmail.com>
2020-04-26 14:03:15 -0400

    Integrate google analytics

--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,20 @@
-export const ENV = "dev";
+export const ENV = 'dev';
 
-export const SERVER_URL = "http://localhost:8000";
+export const SERVER_URL = 'http://localhost:8000';
 
 export const CONFIG_SERVER_URL =
-  "https://raw.githubusercontent.com/ChrisXJH/config-server/master/uw_course_explorer";
+  'https://raw.githubusercontent.com/ChrisXJH/config-server/master/uw_course_explorer';
 
 export const FEATURE_FLAG_SERVER_URL =
-  "https://raw.githubusercontent.com/ChrisXJH/config-server/master/uw_course_explorer/feature_flags/dev.json";
+  'https://raw.githubusercontent.com/ChrisXJH/config-server/master/uw_course_explorer/feature_flags/dev.json';
 
 export const FACEBOOK_APP_ID = 507086496600584;
 
+export const GA_TRACKING_ID = 'UA-112067320-3';
+
 export const configList = [
   {
-    key: "popularSubjects",
+    key: 'popularSubjects',
     url: `${CONFIG_SERVER_URL}/subjects/popular_subjects.json`
   }
 ];
