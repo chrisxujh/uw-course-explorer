7f4f4f078836ee1aa69bf24bdc89cab85487e67d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-07-07 06:38:42 +0000

    Deploy chrisxjh/uw-course-explorer to github.com/chrisxjh/uw-course-explorer.git:gh-pages

---
+++ precache-manifest.891db54e08f68298b77c97c171b13eb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdc3414dbfddd992b8737c4c864e528",
+    "url": "/uw-course-explorer/index.html"
+  },
+  {
+    "revision": "ccf18e14823d93280baf",
+    "url": "/uw-course-explorer/static/css/main.8c31a355.chunk.css"
+  },
+  {
+    "revision": "37869713a56e4f894287",
+    "url": "/uw-course-explorer/static/js/2.dba6c9ca.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/uw-course-explorer/static/js/2.dba6c9ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf18e14823d93280baf",
+    "url": "/uw-course-explorer/static/js/main.f28afd23.chunk.js"
+  },
+  {
+    "revision": "4b13fd1cab3d7b89ca90",
+    "url": "/uw-course-explorer/static/js/runtime-main.a2c56d8b.js"
+  }
+]);
